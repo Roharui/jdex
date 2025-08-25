44a5e4ea67ff1e1f474f83c604648422d473fd8a
Roharui <jack555586@gmail.com>
2025-08-25 20:32:38 +0900

    이름 변경

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from jdex import app, db
+from app import app, db
 
-from jdex.models import User # User 모델 임포트
+from app.models import User # User 모델 임포트
 
 with app.app_context():
     # 데이터베이스 스키마 생성
