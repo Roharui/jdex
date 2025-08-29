26adae532937d0d0fcef827914cebbe1411fb980
nanunsalameyam <kimsihwan2020@gmail.com>
2025-08-29 22:56:18 +0900

    뭔가 많이 함

--- app/views/example.py
+++ app/views/example.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import render_template, session, redirect
 
-from .. import app
+from .. import app, db
 from ..models.users import User
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if not session.get('user_id'):
+        return redirect('/login')
+    user = db.session.execute(db.select(User).filter_by(id=session.get('user_id'))).scalar_one_or_none()
+    return render_template('index.html', point=user.point)
 
 @app.route('/about')
 def about():
